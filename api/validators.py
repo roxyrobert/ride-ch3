4c7799c36b43571b68073127cdf93882e2f20108
roxyrobert <robert.ssebintu@gmail.com>
2018-06-28 19:08:21 +0300

    add join ride schema

--- api/validators.py
+++ api/validators.py
@@ -9,3 +9,12 @@ schema = {
     },
     "required":["route", "driver", "fare"]
 }
+
+join_ride_schema = {
+    "type":"object",
+    "properties": {
+        "username":{"type":"string"},
+        "contact":{"type":"string", "pattern":"^[0-9]{4}-[0-9]{6}"}
+    },
+    "required":["username", "contact"]
+}
