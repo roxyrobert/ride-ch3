370e159356d52ef897c8bbd16c876da6a35c9716
roxyrobert <robert.ssebintu@gmail.com>
2018-06-30 22:51:40 +0300

    Add database connection

--- api/__init__.py
+++ api/__init__.py
@@ -1,3 +1,12 @@
+import psycopg2
 from flask import Flask
+
 app = Flask(__name__)
+
+# Connect to an existing database
+conn = psycopg2.connect("dbname=ride user=roxy")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
 from api import views
