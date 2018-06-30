4dc98733059c16acd00137935220aca3487a9675
roxyrobert <robert.ssebintu@gmail.com>
2018-06-30 22:52:02 +0300

    Create database file

---
+++ api/db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users(
+   id SERIAL PRIMARY KEY,
+   username VARCHAR(100) NOT NULL,
+   email VARCHAR(100) NOT NULL,
+   password VARCHAR(100) NOT NULL,
+   contact VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE rides(
+   id SERIAL PRIMARY KEY,
+   route VARCHAR(100) NOT NULL,
+   driver INT references users(id),
+   fare VARCHAR(100) NOT NULL,
+   created_at TIMESTAMP default CURRENT_TIMESTAMP
+);
+
+CREATE TABLE requests(
+   id SERIAL PRIMARY KEY,
+   passenger INT references users(id),
+   ride INT references rides(id),
+   created_at TIMESTAMP default CURRENT_TIMESTAMP
+);
