d165fd1efd7b8dce1e11ab24a9ee7910146258fe
roxyrobert <robert.ssebintu@gmail.com>
2018-07-04 20:45:01 +0300

    modify validation parameters

--- api/validators.py
+++ api/validators.py
@@ -3,9 +3,12 @@ from jsonschema import validate
 users_schema = {
     "type": "object",
     "properties": {
-        "username": {"type": "string"},
-        "email": {"type": "string", "format": "email"},
-        "password": {"type": "string"},
+        "username": {"type": "string", "maxLength": 10,
+            "minLength": 3},
+        "email": {"type": "string", "maxLength": 30,
+            "minLength": 5},
+        "password": {"type": "string", "maxLength": 30,
+            "minLength": 8},
         "contact": {"type": "string", "pattern": "^[0-9]{4}-[0-9]{6}"}
     },
     "required": ["username", "email", "password", "contact"]
@@ -14,8 +17,10 @@ users_schema = {
 login_schema = {
     "type": "object",
     "properties": {
-        "email": {"type": "string", "format": "email"},
-        "password": {"type": "string"}
+        "email": {"type": "string", "maxLength": 30,
+            "minLength": 6},
+        "password": {"type": "string", "maxLength": 30,
+            "minLength": 3}
     },
     "required": ["email", "password"]
 }
@@ -23,8 +28,10 @@ login_schema = {
 create_ride_schema = {
     "type": "object",
     "properties": {
-        "route": {"type": "string"},
-        "driver": {"type": "string"},
+        "route": {"type": "string", "maxLength": 10,
+            "minLength": 3},
+        "driver": {"type": "string", "maxLength": 10,
+            "minLength": 1},
         "fare": {"type": "integer"}
     },
     "required": ["route", "driver", "fare"]
@@ -33,8 +40,10 @@ create_ride_schema = {
 join_ride_schema = {
     "type": "object",
     "properties": {
-        "passenger": {"type": "string"},
-        "ride": {"type": "string"}
+        "passenger": {"type": "string", "maxLength": 10,
+            "minLength": 1},
+        "ride": {"type": "string", "maxLength": 10,
+            "minLength": 1}
     },
     "required": ["passenger", "ride"]
 }
