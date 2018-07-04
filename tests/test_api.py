aedba1c52764befb82e28589fd53c1316bb8ec79
roxyrobert <robert.ssebintu@gmail.com>
2018-07-04 09:42:21 +0300

    test login endoint with invalid data

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,7 +13,7 @@ class RideTestCase(TestCase):
         }
     
     def create_app(self):
-        initialize the test client
+        #initialize the test client
         return app
 
     def test_create_ride(self):
@@ -141,6 +141,7 @@ class RideTestCase(TestCase):
         self.assertEqual(res.status_code, 400)
         self.assertNotEqual(res.status_code, 201)
 
+    # Test login with valid data inputs
     def test_login(self):
         self.sample_data = {
             'username':'tester',
@@ -169,6 +170,35 @@ class RideTestCase(TestCase):
         self.assertIn('status', res_data)
         self.assertIn('message', res_data)
         self.assertIn('access_token', res_data)
+    
+    # Test login with invalid login data
+    def test_login(self):
+        self.sample_data = {
+            'username':'tester',
+            'email': 'tester@mail.com',
+            'password':'12345',
+            'contact':'0702-811121'
+        }
+        signup_data = self.client.post('/auth/signup',
+                            data=json.dumps(self.sample_data),
+                            content_type='application/json')
+        response_data = json.loads(signup_data.data.decode())
+        self.sample_data = {
+            'email': 'tester.com',
+            'password':'12345'
+        }
+        # pick client data and decode it
+        res = self.client.post('/auth/login',
+                            data=json.dumps(self.sample_data),
+                            content_type='application/json')
+        res_data = json.loads(res.data.decode())
+        # assert expected status
+        self.assertEqual(res_data['status'], 'fail')
+        # assert expected status code
+        self.assertEqual(res.status_code, 400)
+        # assert keys
+        self.assertIn('status', res_data)
+        self.assertIn('message', res_data)
     
     def test_get_requests_by_id(self):
         self.sample_data = {
