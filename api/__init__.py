582575b8f3c097bd4e9e04f407f59c7471c31d9a
roxyrobert <robert.ssebintu@gmail.com>
2018-07-05 23:37:23 +0300

    Updated Init.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,5 @@
-import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
-
-# Connect to an existing database
-conn = psycopg2.connect("dbname=ride user=roxy")
-
-# Open a cursor to perform database operations
-cur = conn.cursor()
 
 from api import views
