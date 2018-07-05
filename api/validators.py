b919a6a1aad38f424acee41e45ee6f5d459daea0
roxyrobert <robert.ssebintu@gmail.com>
2018-07-05 23:39:22 +0300

    Update validators

--- api/validators.py
+++ api/validators.py
@@ -1,14 +1,13 @@
-from jsonschema import validate
 
 users_schema = {
     "type": "object",
     "properties": {
         "username": {"type": "string", "maxLength": 10,
-            "minLength": 3},
+                     "minLength": 3},
         "email": {"type": "string", "maxLength": 30,
-            "minLength": 5},
+                  "minLength": 5},
         "password": {"type": "string", "maxLength": 30,
-            "minLength": 8},
+                     "minLength": 8},
         "contact": {"type": "string", "pattern": "^[0-9]{4}-[0-9]{6}"}
     },
     "required": ["username", "email", "password", "contact"]
@@ -18,9 +17,9 @@ login_schema = {
     "type": "object",
     "properties": {
         "email": {"type": "string", "maxLength": 30,
-            "minLength": 6},
+                  "minLength": 6},
         "password": {"type": "string", "maxLength": 30,
-            "minLength": 3}
+                     "minLength": 3}
     },
     "required": ["email", "password"]
 }
@@ -29,9 +28,9 @@ create_ride_schema = {
     "type": "object",
     "properties": {
         "route": {"type": "string", "maxLength": 10,
-            "minLength": 3},
+                  "minLength": 3},
         "driver": {"type": "integer", "multipleOf": 1.0, "minimum": 1},
-        "fare": {"type": "integer"}
+        "fare": {"type": "integer", "multipleOf": 1.0, "minimum": 1}
     },
     "required": ["route", "driver", "fare"]
 }
